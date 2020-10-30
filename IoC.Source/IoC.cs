96fa6d3eed3f8db750d2501227216b3dd5e5e499
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-10-30 15:20:14 +0300

    Minor changes in TaskFeature

--- IoC.Source/IoC.cs
+++ IoC.Source/IoC.cs
@@ -9670,31 +9670,31 @@ namespace IoC.Features
     [PublicAPI]
     public sealed  class TaskFeature : IConfiguration
     {
+        [CanBeNull] private readonly TaskScheduler _taskScheduler;
+
         /// The default instance.
-        public static readonly IConfiguration Set = new TaskFeature();
+        public static readonly IConfiguration Set = new TaskFeature(TaskScheduler.Current);
 
-        /// <summary>
-        /// TaskFeature default tag
-        /// </summary>
-        public static readonly Tag Tag = new Tag(Guid.NewGuid());
-
-        private TaskFeature() { }
+        public TaskFeature([CanBeNull] TaskScheduler taskScheduler = null) => _taskScheduler = taskScheduler;
 
         /// <inheritdoc />
         public IEnumerable<IToken> Apply(IMutableContainer container)
         {
             if (container == null) throw new ArgumentNullException(nameof(container));
-            yield return container.Register(ctx => TaskScheduler.Current);
-            yield return container.Register(ctx => CreateTask(ctx.Container.Inject<Func<TT>>(ctx.Key.Tag), ctx.Container.Inject<TaskScheduler>()), null, AnyTag);
+            yield return container.Register(ctx => CreateTask(ctx.Container.Inject<Func<TT>>(ctx.Key.Tag), ctx.Container.TryInject<TaskScheduler>() ?? _taskScheduler), null, AnyTag);
 #if !NET40 && !NET403 && !NET45 && !NET45 && !NET451 && !NET452 && !NET46 && !NET461 && !NET462 && !NET47 && !NET48 && !NETCOREAPP1_0 && !NETCOREAPP1_1 && !NETSTANDARD1_0 && !NETSTANDARD1_1 && !NETSTANDARD1_2&& !NETSTANDARD1_3 && !NETSTANDARD1_4 && !NETSTANDARD1_5 && !NETSTANDARD1_6 && !NETSTANDARD2_0 && !WINDOWS_UWP
             yield return container.Register(ctx => new ValueTask<TT>(ctx.Container.Inject<TT>(ctx.Key.Tag)), null, AnyTag);
 #endif
         }
 
-        private static Task<T> CreateTask<T>(Func<T> factory, TaskScheduler taskScheduler)
+        private static Task<T> CreateTask<T>([NotNull] Func<T> factory, [CanBeNull] TaskScheduler taskScheduler)
         {
             var task = new Task<T>(factory);
-            task.Start(taskScheduler);
+            if (taskScheduler != null)
+            {
+                task.Start(taskScheduler);
+            }
+
             return task;
         }
     }
